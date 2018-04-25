9cfcf1f1a18abc318ee7f28ed661679b74c635e2
Nikita Mediankin <mediankin@ibis.ms.mff.cuni.cz>
2018-04-25 09:18:37 +0200

    Corrected stuff concerning training the baseline

--- pilot/train-baseline.sh
+++ pilot/train-baseline.sh
@@ -4,5 +4,8 @@ COMMON=/net/work/people/mediankin/snmt
 DATA=${COMMON}/data/czeng/pilot
 MONKEY=${COMMON}/neuralmonkey
 
-source ${COMMON}/nmenv/bin/activate
+source ~/.bashrc
+source activate tf15
+#source ${COMMON}/nmenv/bin/activate
+#source /a/merkur3/kocmanek/ANACONDA/envs/tf15/bin/activate
 ${MONKEY}/bin/neuralmonkey-train ${COMMON}/monkey-business/pilot/baseline.ini
